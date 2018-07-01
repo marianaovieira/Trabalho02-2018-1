c0207cc3f892b620badbf161820be94948d8a175
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 19:32:24 -0300

    Criação da activity CandidatoDetalhe
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ListaCandidato.java
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ListaCandidato.java
@@ -1,15 +1,38 @@
 package br.iff.pooa20181.trabalho02_2018_1.activity;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import br.iff.pooa20181.trabalho02_2018_1.CandidatoDetalhe;
 import br.iff.pooa20181.trabalho02_2018_1.R;
+import io.realm.Realm;
 
 public class ListaCandidato extends AppCompatActivity {
+
+    private Realm realm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_lista_candidato );
+
+        realm = Realm.getDefaultInstance();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( ListaCandidato.this, CandidatoDetalhe.class );
+
+                intent.putExtra( "id", 0 );
+                startActivity( intent );
+            }
+        } );
     }
-}
+
+    }
+
