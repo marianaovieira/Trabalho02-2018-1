29c408edd1849ccad4c08a5f7c3efb51fcd2992b
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 19:21:54 -0300

    Correção tela Cadastro Eleitor
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ContentListaEleitor.java
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ContentListaEleitor.java
@@ -1,7 +1,9 @@
-package br.iff.pooa20181.trabalho02_2018_1;
+package br.iff.pooa20181.trabalho02_2018_1.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.iff.pooa20181.trabalho02_2018_1.R;
 
 public class ContentListaEleitor extends AppCompatActivity {
 
