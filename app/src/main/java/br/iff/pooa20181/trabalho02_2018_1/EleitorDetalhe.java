c1420767bf8776be571a27b17d8f9ad9c2b88496
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 14:41:34 -0300

    Configuração ListaEleitor.class
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/EleitorDetalhe.java
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/EleitorDetalhe.java
@@ -1,13 +1,57 @@
 package br.iff.pooa20181.trabalho02_2018_1;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+
+import br.iff.pooa20181.trabalho02_2018_1.model.Eleitor;
+import io.realm.Realm;
 
 public class EleitorDetalhe extends AppCompatActivity {
 
+    EditText edNome, edNome_mae, edData, edNumeroTitulo, edZona, edCidade, edSecao;
+
+    Button btnSalvar, btnAlterar, btnExcluir;
+
+    int id;
+    Eleitor eleitor;
+    private Realm realm;
+
+    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_eleitor_detalhe );
+
+        edNome = (EditText) findViewById( R.id.txtNomeEleitor );
+        edNome_mae = (EditText) findViewById( R.id.txtNomeMaeEleitor );
+        edData = (EditText) findViewById( R.id.txtDataEleitor );
+        edNumeroTitulo = (EditText) findViewById( R.id.txtNumeroTituloEleitor );
+        edZona = (EditText) findViewById( R.id.txtZonaEleitor );
+        edCidade = (EditText) findViewById( R.id.txtCidadeEleitor );
+        edSecao = (EditText) findViewById( R.id.txtSecaoEleitor );
+
+        btnSalvar = (Button) findViewById( R.id.btnSalvar );
+        btnAlterar = (Button) findViewById( R.id.btnAlterar );
+        btnExcluir = (Button) findViewById( R.id.btnExcluir );
+
+
+
+        Intent intent = getIntent();
+        id = (int) intent.getSerializableExtra("id");
+        realm = Realm.getDefaultInstance();
+
     }
+
+
+
+
 }
--- app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ListaEleitor.java
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ListaEleitor.java
@@ -1,32 +1,38 @@
+package br.iff.pooa20181.trabalho02_2018_1.activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toolbar;
 
+
+import br.iff.pooa20181.trabalho02_2018_1.EleitorDetalhe;
 import br.iff.pooa20181.trabalho02_2018_1.R;
 import io.realm.Realm;
 
-public class ListagemEleitorActivity extends AppCompatActivity implements ClickRecyclerViewListener {
+public class ListaEleitor extends AppCompatActivity {
 
     private Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_eleitor);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_lista_eleitor );
 
 
         realm = Realm.getDefaultInstance();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ListagemEleitorActivity.this,EleitorDetalhe.class);
+                Intent intent = new Intent( ListaEleitor.this, EleitorDetalhe.class );
 
-                intent.putExtra("id",0);
-                startActivity(intent);
+                intent.putExtra( "id", 0 );
+                startActivity( intent );
             }
-        });
+        } );
     }
+
+}
