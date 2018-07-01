335f4ffb3ec378105cce22ae6ef888aca633f217
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 19:26:37 -0300

    Criação da activity ListaCandidato
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

---
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/activity/ListaCandidato.java
@@ -0,0 +1,15 @@
+package br.iff.pooa20181.trabalho02_2018_1.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.iff.pooa20181.trabalho02_2018_1.R;
+
+public class ListaCandidato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_lista_candidato );
+    }
+}
