c240317d9f93e9a9bf2d553c547a637f2b3dab16
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 18:03:55 -0300

    Configuração EleitorDetalhe.class
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/EleitorDetalhe.java
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/EleitorDetalhe.java
@@ -5,10 +5,14 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import br.iff.pooa20181.trabalho02_2018_1.model.Eleitor;
 import io.realm.Realm;
@@ -49,9 +53,131 @@ public class EleitorDetalhe extends AppCompatActivity {
         id = (int) intent.getSerializableExtra("id");
         realm = Realm.getDefaultInstance();
 
+
+        if(id != 0)
+        {
+
+            btnSalvar.setEnabled(false);
+            btnSalvar.setClickable(false);
+            btnSalvar.setVisibility( View.INVISIBLE);
+
+            btnAlterar.setEnabled(true);
+            btnAlterar.setClickable(true);
+            btnAlterar.setVisibility(View.VISIBLE);
+
+
+            eleitor = realm.where(Eleitor.class).equalTo("id",id).findFirst();
+
+            edNome.setText(eleitor.getNome());
+            edNome_mae.setText(eleitor.getNome_mae());
+            edNumeroTitulo.setText(eleitor.getNumero_titulo());
+            edData.setText(formato.format((Date) eleitor.getData_nascimento()));
+            edSecao.setText(eleitor.getSecao_eleitoral());
+            edZona.setText(eleitor.getZona_eleitoral());
+            edCidade.setText(eleitor.getCidade());
+        } else
+        {
+
+            btnAlterar.setEnabled(false);
+            btnAlterar.setClickable(false);
+            btnAlterar.setVisibility(View.INVISIBLE);
+            btnExcluir.setEnabled(false);
+            btnExcluir.setClickable(false);
+            btnExcluir.setVisibility(View.INVISIBLE);
+        }
+
+        btnSalvar.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                salvar();
+            }
+        });
+        btnAlterar.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                alterar();
+            }
+        });
+        btnExcluir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                deletar();
+            }
+        });
+
+
+
     }
 
 
+    public void deletar(){
+        realm.beginTransaction();
+        eleitor.deleteFromRealm();
+        realm.commitTransaction();
+        realm.close();
+
+        Toast.makeText(this,"Eleitor deletado com sucesso",Toast.LENGTH_LONG).show();
+        this.finish();
+
+    }
+
+    public void salvar() {
 
 
+        int proximoID = 1;
+        if(realm.where(Eleitor.class).max("id") !=null)
+            proximoID = realm.where(Eleitor.class).max("id").intValue()+1;
+
+        realm.beginTransaction();
+        Eleitor eleitor = new Eleitor();
+        eleitor.setId(proximoID);
+        setEGrava(eleitor);
+
+        realm.copyToRealm(eleitor);
+        realm.commitTransaction();
+        realm.close();
+
+        Toast.makeText(this,"Eleitor cadastrado com successo",Toast.LENGTH_LONG).show();
+        this.finish();
+
+    }
+
+    private void setEGrava(Eleitor eleitor){
+
+        eleitor.setNome(edNome.getText().toString());
+        eleitor.setNome_mae(edNome_mae.getText().toString());
+        eleitor.setNumero_titulo(edNumeroTitulo.getText().toString());
+        eleitor.setSecao_eleitoral(edSecao.getText().toString());
+        eleitor.setZona_eleitoral(edZona.getText().toString());
+        eleitor.setCidade(edCidade.getText().toString());
+
+        try {
+            eleitor.setData_nascimento((Date) formato.parse(edData.getText().toString()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void alterar() {
+
+        realm.beginTransaction();
+
+        setEGrava(eleitor);
+
+        realm.copyToRealm(eleitor);
+        realm.commitTransaction();
+        realm.close();
+
+        Toast.makeText(this,"Eleitor alterado!",Toast.LENGTH_LONG).show();
+        this.finish();
+
+    }
 }
+
+
+
+
+
