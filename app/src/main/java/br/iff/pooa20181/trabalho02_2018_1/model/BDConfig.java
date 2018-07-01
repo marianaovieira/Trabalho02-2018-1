f3fdd4633fb21e4d2fe7eaa5cac98d67ec959bbe
Mariana Vieira <marianaovieira1@gmail.com>
2018-07-01 13:00:33 -0300

    Configuração BD

---
+++ app/src/main/java/br/iff/pooa20181/trabalho02_2018_1/model/BDConfig.java
@@ -0,0 +1,22 @@
+package br.iff.pooa20181.trabalho02_2018_1.model;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class BDConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+        builder.name("eleicao.realm");
+        builder.schemaVersion(0);
+        builder.deleteRealmIfMigrationNeeded();
+        RealmConfiguration realmConfiguration = builder.build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
